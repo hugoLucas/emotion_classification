b801333bf89e10ed057fda8513ae5d3ad3997bc4
hugolucas <hugojlucas@gmail.com>
2018-03-24 23:54:15 -0700

    fixed error in file name loader

--- data.py
+++ data.py
@@ -1,18 +1,26 @@
 from torch.utils.data import Dataset
+from librosa.core import load
 from os import listdir, path
+from numpy import pad
 
 
 class AudioData(Dataset):
     def __init__(self, configs):
         self.configs = configs
-
         self.file_list = self.load_files()
 
     def __getitem__(self, index):
-        pass
+        file_path = self.file_list[index]
+        audio_data, _ = load(file_path, sr=self.configs.audio_sample_rate, duration=self.configs.audio_max_length)
+
+        if audio_data.size < self.configs.audio_max_length:
+            pad_length = self.configs.audio_max_length - audio_data.size
+            pad(audio_data, (0, pad_length), 'constant', constant_values=0)
+
+        return audio_data
 
     def __len__(self):
-        pass
+        return len(self.file_list)
 
     def load_files(self):
         """
@@ -28,6 +36,8 @@ class AudioData(Dataset):
         for fld in folders:
             fld_path = path.join(self.configs.data_dir, fld)
             files = filter(lambda f: self.configs.file_type in f, listdir(fld_path))
-            file_list.extend(path.join(fld_path, files))
+
+            for fl in files:
+                file_list.append(path.join(fld_path, fl))
 
         return file_list
