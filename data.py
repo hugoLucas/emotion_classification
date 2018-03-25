362b568ca44bd67dc19ba48673aeb0a70970dfd1
hugolucas <hugojlucas@gmail.com>
2018-03-25 00:05:32 -0700

    get method now properly pads arrays that are too small

--- data.py
+++ data.py
@@ -1,21 +1,23 @@
 from torch.utils.data import Dataset
 from librosa.core import load
 from os import listdir, path
-from numpy import pad
+from numpy import pad, zeros
 
 
 class AudioData(Dataset):
     def __init__(self, configs):
         self.configs = configs
         self.file_list = self.load_files()
+        self.max_samples = self.configs.audio_max_length * self.configs.audio_sample_rate
 
     def __getitem__(self, index):
         file_path = self.file_list[index]
         audio_data, _ = load(file_path, sr=self.configs.audio_sample_rate, duration=self.configs.audio_max_length)
 
-        if audio_data.size < self.configs.audio_max_length:
-            pad_length = self.configs.audio_max_length - audio_data.size
-            pad(audio_data, (0, pad_length), 'constant', constant_values=0)
+        if audio_data.size < self.max_samples:
+            temp_array = zeros(self.max_samples)
+            temp_array[:audio_data.size] = audio_data
+            audio_data = temp_array
 
         return audio_data
 
@@ -32,7 +34,7 @@ class AudioData(Dataset):
         """
         file_list = []
 
-        folders = filter(lambda f: self.configs.folder_tag in f, listdir(self.configs.data_dir))
+        folders = filter(lambda f: self.configs.folder_tag in f and '.' not in f, listdir(self.configs.data_dir))
         for fld in folders:
             fld_path = path.join(self.configs.data_dir, fld)
             files = filter(lambda f: self.configs.file_type in f, listdir(fld_path))
