c7173cc560b7e184e1e6edcf4af1c0b884486948
hugolucas <hugojlucas@gmail.com>
2018-03-27 19:38:14 -0700

    got rid of softmax layer, causes error if you use softmax and crossentropyloss

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from torch.nn import Module, LSTM, Linear, ReLU, Softmax
+from torch.nn import Module, LSTM, Linear, ReLU
 from torch.autograd.variable import Variable
 from torch import randn
 
@@ -15,7 +15,6 @@ class BidirectionalLSTM(Module):
                               out_features=self.configs.dense_1_output_dim)
         self.reul = ReLU()
         self.dense_3 = Linear(in_features=self.configs.dense_1_output_dim, out_features=self.configs.dense_2_output_dim)
-        self.softmax = Softmax()
 
     def forward(self, x):
         dim1, dim3 = self.configs.lstm_layers * 2 * self.configs.batch_size, self.configs.lstm_output_dim
@@ -27,5 +26,5 @@ class BidirectionalLSTM(Module):
         for val in x[0]:
             out, hidden = self.lstm_1(val.view(1, 1, -1), hidden)
         x = self.reul(self.dense_2(out))
-        x = self.softmax(self.dense_3(x))
-        return x
+        x = self.dense_3(x)
+        return x.view(1, -1)
