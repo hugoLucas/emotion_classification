b0a5921a7db8fdb6ea8a8c5750860aeaba24e01b
hugolucas <hugojlucas@gmail.com>
2018-04-11 21:50:34 -0700

    i think this works now

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 from torch.nn import Module, LSTM, Linear
 from torch.nn.functional import relu
-from torch import cat
 
 
 class BidirectionalLSTM(Module):
@@ -13,25 +12,15 @@ class BidirectionalLSTM(Module):
         super(BidirectionalLSTM, self).__init__()
 
         self.configs = model_configs
+        self.n_features = self.configs.audio_sample_rate * self.configs.audio_max_length
         self.lstm_1 = LSTM(input_size=1, hidden_size=self.configs.lstm_output_dim,
-                           num_layers=self.configs.lstm_layers, batch_first=False, dropout=1, bidirectional=True)
+                           num_layers=self.configs.lstm_layers, batch_first=True, dropout=1, bidirectional=True)
         self.dense_2 = Linear(in_features=2 * self.configs.lstm_output_dim,
                               out_features=self.configs.dense_1_output_dim)
         self.dense_3 = Linear(in_features=self.configs.dense_1_output_dim, out_features=self.configs.dense_2_output_dim)
 
     def forward(self, x):
-        x = x.view(self.configs.batch_size, -1, 1)
-        x, hidden = self.lstm_1(x)
-
-        y = None
-        for i in range(0, self.configs.batch_size):
-            y_prime = cat([x[i,0,0:self.configs.lstm_output_dim], x[i,-1,self.configs.lstm_output_dim:]])
-            y_prime = y_prime.view(1, -1)
-            if i == 0:
-                y = y_prime
-            else:
-                y = cat([y, y_prime], dim=0)
-
-        x = relu(self.dense_2(y))
+        x, _ = self.lstm_1(x)
+        x = relu(self.dense_2(x[:, -1, :]))
         x = self.dense_3(x)
         return x
