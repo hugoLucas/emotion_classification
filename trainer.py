501f68994c2e66cc204417391b1ef0fcb7340c4b
hugolucas <hugojlucas@gmail.com>
2018-04-12 20:13:01 -0700

    edited to make compatible with new logger class

--- trainer.py
+++ trainer.py
@@ -5,21 +5,20 @@ from os.path import isfile
 
 
 class AudioTrainer:
-    def __init__(self, configs, audio_model, audio_data, audio_loss, audio_optimizer, save_path=None):
+    def __init__(self, configs, model, loader, loss_fn, optimizer, logger, save_path=None):
         self.configs = configs
-        self.model = audio_model
-        self.data = audio_data
-        self.loss = audio_loss
-        self.optimizer = audio_optimizer
+        self.model = model
+        self.data = loader
+        self.loss = loss_fn
+        self.optimizer = optimizer
+        self.logger = logger
 
         self.save_path = save_path
 
     def train(self):
-        historical_accuracy_data, historical_loss_data = {}, {}
-
         for epoch in range(0, self.configs.epochs):
 
-            accuracy_data, loss_data, iterations = [], [], 0
+            accuracy_sum, loss_sum, iter = 0, 0, 0
             for i, datum in enumerate(self.data):
 
                 # Load inputs and make them cuda accessible
@@ -43,14 +42,19 @@ class AudioTrainer:
                 _, max_index = topk(outputs, k=1)
                 accuracy = sum(eq(squeeze(max_index), labels)).data[0]
 
-                # Add data to current iterations results
-                accuracy_data.append(accuracy)
-                loss_data.append(loss.data[0])
-                iterations += 1
+                # Track epoch results
+                accuracy_sum += accuracy
+                loss_sum += loss.data[0]
+                iter += 1
 
-            # Log results
-            historical_accuracy_data[epoch] = sum_list(accuracy_data)/(iterations * self.configs.batch_size)
-            historical_loss_data[epoch] = sum_list(loss_data)/(iterations * self.configs.batch_size)
+                # Log iteration results
+                self.log_batch_results(accuracy, loss.data[0], iter, epoch)
+
+                if iter == self.configs.iterations:
+                    break
+
+            # Log epoch results
+            self.log_epoch_results(accuracy_sum, loss_sum, iter, epoch)
 
             # Save model
             if self.save_path is not None:
@@ -58,7 +62,17 @@ class AudioTrainer:
 
             print("Epoch {} complete.".format(epoch))
 
-        return historical_accuracy_data, historical_loss_data
+            self.logger.export()
+        self.logger.close()
+
+    def log_epoch_results(self, accuracy_sum, loss_sum, iter, epoch):
+        self.logger.add_data_point(self.configs.avg_acc, accuracy_sum/iter, epoch)
+        self.logger.add_data_point(self.configs.avg_loss, loss_sum/iter, epoch)
+
+    def log_batch_results(self, accuracy, loss, iter, epoch):
+        n_iter = (epoch * self.configs.iterations) + iter
+        self.logger.add_data_point(self.configs.inst_acc, accuracy, n_iter)
+        self.logger.add_data_point(self.configs.inst_loss, loss, n_iter)
 
     def load_model(self):
         """
