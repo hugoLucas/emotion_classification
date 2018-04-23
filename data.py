163dc7f65512c57b2d4877ae7c90118b9eef79ef
hugolucas <hugojlucas@gmail.com>
2018-04-22 17:45:21 -0700

    added mfcc feature extraction method

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
+from librosa.feature import mfcc, delta
+from numpy import concatenate, swapaxes
 from torch.utils.data import Dataset
-from numpy import zeros
 from librosa.core import load
 from os import listdir, path
 
@@ -8,22 +9,20 @@ class AudioData(Dataset):
     def __init__(self, configs):
         self.configs = configs
         self.file_list = self.load_files()
-        self.max_samples = self.configs.audio_max_length * self.configs.audio_sample_rate
+        self.max_samples = None
 
     def __getitem__(self, index):
         file_path = self.file_list[index]
-        audio_data, _ = load(file_path, sr=self.configs.audio_sample_rate, duration=self.configs.audio_max_length)
+        audio_data, sr = load(file_path, duration=self.configs.audio_max_length)
 
-        # Pad audio data if not long enough
-        if audio_data.size < self.max_samples:
-            temp_array = zeros(self.max_samples)
-            temp_array[:audio_data.size] = audio_data
-            audio_data = temp_array
+        # Extract audio features
+        audio_data = self.process_audio(audio_data, sr)
 
-        # Extract label out
+        # Extract label out, subtract one to make first label 0 rather than 1
         file_name = self.file_list[index].split()[-1]
-        label = int(file_name.split('-')[self.configs.emotion_index])
-        return audio_data.reshape(-1, 1), label - 1
+        label = int(file_name.split('-')[self.configs.emotion_index]) - 1
+
+        return audio_data, label
 
     def __len__(self):
         return len(self.file_list)
@@ -47,3 +46,25 @@ class AudioData(Dataset):
                 file_list.append(path.join(fld_path, fl))
 
         return file_list
+
+    @staticmethod
+    def process_audio(audio_data, sr):
+        """
+        Computes the Mel-Frequency Cepstral Coefficients and their first and second order derivatives. Concatenates then
+        all into a single numpy array and the swaps the axis from [n_mfcc, n_samples] to [n_samples, n_mfcc].
+
+        :param audio_data: floating point time series of an audio file
+        :param sr: the sample rate at which audio_data was loaded
+        :return: a feature array of dimension [n_samples, n_mfcc] containing the computed MFCCs and their time
+                 derivatives
+        """
+        mel_freq_coeff = mfcc(y=audio_data, sr=sr, n_mfcc=13, hop_length=int(0.20*sr), n_fft=int(.10*sr))
+        mel_freq_coeff = mel_freq_coeff[1:, :]
+
+        mel_freq_coeff_delta = delta(mel_freq_coeff)
+        mel_freq_coeff_delta_delta = delta(mel_freq_coeff, order=2)
+
+        features = concatenate((mel_freq_coeff, mel_freq_coeff_delta, mel_freq_coeff_delta_delta), axis=0)
+        features = swapaxes(features, 0, 1)
+        return features
+
