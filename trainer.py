2fa08ca3023cf9f14ff992a1a2319217547abf52
hugolucas <hugojlucas@gmail.com>
2018-04-23 14:30:47 -0700

    fixed many silly mistakes i made

--- trainer.py
+++ trainer.py
@@ -1,5 +1,6 @@
 from torch import save, load, topk, eq, squeeze, sum, FloatTensor
 from torch.autograd.variable import Variable
+from numpy import zeros
 from os import path
 
 
@@ -8,7 +9,7 @@ class AudioTrainer:
                  test_loader=None):
         self.configs = configs
         self.model = model
-        self.data = train_loader
+        self.train_data = train_loader
         self.loss = loss_fn
         self.optimizer = optimizer
         self.logger = logger
@@ -17,7 +18,7 @@ class AudioTrainer:
         if self.load_path is not None:
             self.save_path = self.load_path if save_path is None else save_path
 
-        self.test_loader = test_loader
+        self.test_data = test_loader
 
     def train(self):
         for epoch in range(0, self.configs.epochs):
@@ -25,41 +26,28 @@ class AudioTrainer:
             accuracy_sum, loss_sum, n_iter = 0, 0, 0
             while n_iter < self.configs.iterations:
                 try:
-                    for i, datum in enumerate(self.data):
+                    for i, datum in enumerate(self.train_data):
 
-                        # Load inputs and make them cuda accessible
-                        inputs, labels = self.convert_data(datum)
-
-                        # Skip this iteration if the batch is incomplete
-                        if not inputs.shape[0] == labels.shape[0] == self.configs.batch_size:
+                        outputs, labels, loss = self.train_step(datum)
+                        if outputs.shape[0] == 1:
                             continue
 
-                        # Zero out grads, go forward
-                        self.optimizer.zero_grad()
-                        outputs = self.model(inputs)
-
-                        # Calculate loss and propagate backwards
-                        loss = self.loss(outputs, labels)
-                        loss.backward()
-                        self.optimizer.step()
-
-                        # Track epoch results
                         current_accuracy = self.calculate_accuracy(outputs, labels)
                         accuracy_sum += current_accuracy
                         loss_sum += loss.data[0]
                         n_iter += 1
 
-                        # Log iteration results
                         if n_iter % self.configs.log_interval == 0:
                             self.log_batch_results(current_accuracy, loss.data[0], n_iter, epoch)
                             print("{}.{} complete....".format(epoch, n_iter))
-
                         if n_iter >= self.configs.iterations:
                             break
+
                 except TypeError:
                     print("Error on Epoch {}...".format(epoch))
 
-            self.log_epoch_results(accuracy_sum, loss_sum, n_iter, epoch)
+            test_acc = self.test_model()
+            self.log_epoch_results(accuracy_sum, loss_sum, test_acc, n_iter, epoch)
 
             # Save model
             if self.save_path is not None:
@@ -67,6 +55,48 @@ class AudioTrainer:
             print("Epoch {} complete.".format(epoch))
             self.logger.export()
         self.logger.close()
+
+    def test_model(self):
+        accuracy_sum, n_iter = 0, 0
+        while n_iter < self.configs.test_iterations:
+            try:
+                for i, datum in enumerate(self.test_data):
+                    outputs, labels = self.feed_forward(datum)
+                    if outputs.shape[0] == 1:
+                            continue
+                    else:
+                        accuracy_sum += self.calculate_accuracy(outputs, labels)
+                        n_iter += 1
+            except ValueError:
+                print("Error on iter {}...".format(n_iter))
+        return accuracy_sum / (n_iter * self.configs.batch_size)
+
+    def feed_forward(self, datum):
+        # Load inputs and make them cuda accessible
+        inputs, labels = self.convert_data(datum)
+
+        # Skip this iteration if the batch is incomplete
+        if not inputs.shape[0] == labels.shape[0] == self.configs.batch_size:
+            out = zeros((1, 1))
+            return out, out
+
+        # Zero out grads, go forward
+        self.optimizer.zero_grad()
+        outputs = self.model(inputs)
+
+        return outputs, labels
+
+    def train_step(self, datum):
+        outputs, labels = self.feed_forward(datum)
+
+        if outputs.shape[0] == 1:
+            return outputs, labels, labels
+
+        loss = self.loss(outputs, labels)
+        loss.backward()
+        self.optimizer.step()
+
+        return outputs, labels, loss
 
     @staticmethod
     def convert_data(datum):
@@ -83,9 +113,10 @@ class AudioTrainer:
 
         return accuracy
 
-    def log_epoch_results(self, accuracy_sum, loss_sum, iter, epoch):
-        self.logger.add_data_point(self.configs.avg_acc, accuracy_sum/(iter * self.configs.batch_size), epoch)
+    def log_epoch_results(self, accuracy_sum, loss_sum, test_acc, iter, epoch):
+        self.logger.add_data_point(self.configs.avg_acc_train, accuracy_sum/(iter * self.configs.batch_size), epoch)
         self.logger.add_data_point(self.configs.avg_loss, loss_sum/iter, epoch)
+        self.logger.add_data_point(self.configs.avg_acc_test, test_acc, epoch)
 
     def log_batch_results(self, accuracy, loss, iter, epoch):
         n_iter = (epoch * self.configs.iterations) + iter
