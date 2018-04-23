02c58261d86475f2ed649c0ab5e8dbaa2abc1bc0
hugolucas <hugojlucas@gmail.com>
2018-04-23 13:19:03 -0700

    loads different dataset depending on new training_data param

--- data.py
+++ data.py
@@ -6,8 +6,9 @@ from os import listdir, path
 
 
 class AudioData(Dataset):
-    def __init__(self, configs):
+    def __init__(self, configs, training_data=True):
         self.configs = configs
+        self.training_data = training_data
         self.file_list = self.load_files()
         self.max_samples = None
 
@@ -39,7 +40,8 @@ class AudioData(Dataset):
         """
         file_list = []
 
-        folders = filter(lambda f: self.configs.folder_tag in f and '.' not in f, listdir(self.configs.data_dir))
+        repo = self.configs.data_dir if self.training_data else self.configs.test_dir
+        folders = filter(lambda f: self.configs.folder_tag in f and '.' not in f, listdir(repo))
         for fld in folders:
             fld_path = path.join(self.configs.data_dir, fld)
             files = filter(lambda f: self.configs.file_type in f, listdir(fld_path))
@@ -56,7 +58,7 @@ class AudioData(Dataset):
         all into a single numpy array and the swaps the axis from [n_mfcc, n_samples] to [n_samples, n_mfcc].
 
         :param audio_data: floating point time series of an audio file
-        :param sr: the sample rate at which audio_data was loaded
+        :param sr: the sample rate at which train_data was loaded
         :return: a feature array of dimension [n_samples, n_mfcc] containing the computed MFCCs and their time
                  derivatives
         """
