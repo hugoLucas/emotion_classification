0aba5576385c47fbe0743793f3a625cb201950d8
hugolucas <hugojlucas@gmail.com>
2018-04-22 21:25:01 -0700

    switched back to bidirectional model

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils.config_utils import get_config_from_json
 from torch.utils.data import DataLoader
 from torch.nn import CrossEntropyLoss
-from model import SimpleLSTM
+from model import BidirectionalLSTM
 from trainer import AudioTrainer
 from logger import AudioLogger
 from torch.optim import Adam
@@ -16,12 +16,14 @@ RUN_CONFIG_FILE = "config_1.json"
 # Run Configs
 model_configs, _ = get_config_from_json(path.join('./configs', RUN_CONFIG_FILE))
 
-# Data
+# Training Data
 audio_data = AudioData(configs=model_configs)
 train_loader = DataLoader(dataset=audio_data, batch_size=model_configs.batch_size, shuffle=True, num_workers=4)
 
+# Test Data
+
 # Model
-audio_model = SimpleLSTM(model_configs=model_configs)
+audio_model = BidirectionalLSTM(model_configs=model_configs)
 audio_model.cuda()
 
 # Training Params
@@ -33,5 +35,6 @@ logger = AudioLogger("./run.json")
 
 # Train Model
 trainer = AudioTrainer(model_configs, audio_model, train_loader, loss_fn, optimizer, logger,
-                       save_path="./models/new_model2.pt")
+                       load_path="./models/mfcc.pt", save_path="./models/mfcc_2.pt")
+trainer.load_model()
 trainer.train()
