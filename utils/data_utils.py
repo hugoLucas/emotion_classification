ae4c54075fad89b92f9d65d26030a300d40768a5
hugolucas <hugojlucas@gmail.com>
2018-04-23 18:18:40 -0700

    users can now automatically create training and testing dirs

---
+++ utils/data_utils.py
@@ -0,0 +1,32 @@
+from os import path, listdir, makedirs, rename, rmdir
+from utils.config_utils import get_config_from_json
+from random import random
+
+
+def create_directories(configs):
+    train_directory, test_directory = configs.data_dir, configs.test_dir
+    if not path.isdir(train_directory):
+        makedirs(train_directory)
+    if not path.isdir(test_directory):
+        makedirs(test_directory)
+
+    return train_directory, test_directory
+
+
+def move_file(folder_path, destination_path, file_name):
+    rename(path.join(folder_path, file_name), path.join(destination_path, file_name))
+
+
+def process_folders(configs):
+    train_directory, test_directory = create_directories(configs)
+    for folder in filter(lambda v: "." not in v, listdir(configs.raw_data_dir)):
+        folder_path = path.join(configs.raw_data_dir, folder)
+        for file in filter(lambda v: ".wav" in v, listdir(folder_path)):
+            destination = train_directory if random() <= configs.p_train else test_directory
+            move_file(folder_path, destination, file)
+        rmdir(folder_path)
+
+RUN_CONFIG_FILE = "config_1.json"
+model_configs, _ = get_config_from_json(path.join('/home/hugolucas/PycharmProjects/sound/configs', RUN_CONFIG_FILE))
+
+process_folders(model_configs)
