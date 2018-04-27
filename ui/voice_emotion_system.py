9f5e5eda2a36440945fa9bfb10078a2ea4599fc0
hugolucas <hugojlucas@gmail.com>
2018-04-26 20:58:53 -0700

    put entire GUI into a class to make it more tidy

--- ui/voice_emotion_system.py
+++ ui/voice_emotion_system.py
@@ -1,34 +1,71 @@
 from tkinter import *
-from ui.gui_utils import rec, center_window
+from tkinter.messagebox import showinfo
+from utils.model_utils import Predictor
+from ui.gui_utils import RecorderThread, PredictionThread
 
 
-def on_record():
-    global action
-    action.set("Recording...")
-    rec()
-    action.set("Enter answer...")
+class Application:
 
-root = Tk()
-center_window(root, 400, 400)
-root.wm_title('Voice Emotion Recognizer')
+    window_title = "Voice Emotion Recognizer"
+    main_label = "How are you Feeling today?"
+    record_label = "Record"
+    analyze_label = "Analyze"
+    quit_label = "Quit"
 
-theLabel = Label(root, text="How are you Feeling today?", font=("Helvetical", 18), bg="blue")
-theLabel.pack(fill=X)
-topFrame = Frame(root)
-topFrame.pack()
-bottomFrame = Frame(root)
-bottomFrame.pack(side=TOP)
+    def __init__(self):
+        self.root = Tk()
+        self.build_strings()
+        self.build_predictor()
 
-button1 = Button(topFrame, text="RECORD", fg="red", command=on_record)
-button3 = Button(topFrame, text="ANALYSIS", fg="green")
-button4 = Button(bottomFrame, text="QUIT/CANCEL", fg="black", command=root.destroy)
-button1.pack(side=LEFT)
+        self.root.wm_title(self.window_title)
 
-button3.pack(side=LEFT)
-button4.pack(side=RIGHT)
+        label_1 = Label(text=self.main_label, fg="white", font=("Helvetical", 16), bg="blue")
+        label_1.grid(row=0, column=0, columnspan=10, pady=(0, 30))
 
-action = StringVar()
-action.set(value="Enter answer")
-Label(root, text=action.get(), textvariable=action, font=("helvetical", 12)).pack()
+        self.button_1 = Button(self.root, textvariable=self.record_label, fg="red", height=2, command=self.on_record)
+        self.button_1.grid(row=2, column=1, columnspan=3, pady=(0, 10))
 
-root.mainloop()
+        # state=DISABLED
+        self.button_2 = Button(self.root, textvariable=self.analyze_label, fg="black", height=2, command=self.on_analyze)
+        self.button_2.grid(row=2, column=6, columnspan=3, pady=(0, 10))
+
+        self.button_3 = Button(self.root, text=self.quit_label, fg="black", height=2, command=self.root.destroy)
+        self.button_3.grid(row=3, column=4, columnspan=2, pady=(0, 10))
+
+    def on_analyze(self):
+        self.analyze_label.set("Analyzing...")
+
+        pred = PredictionThread(predictor=self.predictor, file_path="./output.wav", callback_fn=self.after_analyze)
+        pred.start()
+
+    def after_analyze(self, emotional_state):
+        self.analyze_label.set("Analyze")
+        showinfo("Emotional State", emotional_state + "    ")
+
+    def on_record(self):
+        self.record_label.set("Recording...")
+
+        recorder = RecorderThread(self.after_record)
+        recorder.start()
+
+    def after_record(self):
+        self.record_label.set("Re-Record")
+        self.button_2['state'] = "normal"
+
+    def build_strings(self):
+        self.window_title = "Voice Emotion Recognizer"
+        self.main_label = "How are you Feeling today?"
+        self.quit_label = "Quit"
+
+        self.record_label = StringVar()
+        self.analyze_label = StringVar()
+
+        self.record_label.set("Record")
+        self.analyze_label.set("Analyze")
+
+    def build_predictor(self):
+        self.predictor = Predictor(config_path="/home/hugolucas/PycharmProjects/sound/configs/config_1.json",
+                                   model_state="/home/hugolucas/PycharmProjects/sound/models/final_model.pt")
+
+app = Application()
+app.root.mainloop()
