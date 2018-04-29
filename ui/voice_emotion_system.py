9e6a69da2dff4c984409eb0f566812d09774d068
hugolucas <hugojlucas@gmail.com>
2018-04-29 16:28:19 -0700

    added some comments

--- ui/voice_emotion_system.py
+++ ui/voice_emotion_system.py
@@ -1,23 +1,23 @@
 from tkinter import *
 from tkinter.messagebox import showinfo
+
+from utils.gui_utils import RecorderThread, PredictionThread
 from utils.model_utils import Predictor
-from ui.gui_utils import RecorderThread, PredictionThread
 
 
 class Application:
-
-    window_title = "Voice Emotion Recognizer"
-    main_label = "How are you Feeling today?"
-    record_label = "Record"
-    analyze_label = "Analyze"
-    quit_label = "Quit"
+    """
+    A simple GUI that allows the User to record themselves and pass the audio data to a trained model.
+    """
 
     def __init__(self):
+        """
+        Creates a Predictor object in order to classify audio at a later time and builds the GUI once that is complete.
+        """
         self.predictor = Predictor(config_path="/home/hugo/PycharmProjects/emotion_classification/configs/config_1.json",
                                    model_state="/home/hugo/PycharmProjects/emotion_classification/final_model.pt")
         self.root = Tk()
         self.build_strings()
-
         self.root.wm_title(self.window_title)
 
         label_1 = Label(text=self.main_label, fg="white", font=("Helvetical", 16), bg="blue")
@@ -34,6 +34,11 @@ class Application:
         self.button_3.grid(row=3, column=4, columnspan=2, pady=(0, 10))
 
     def on_analyze(self):
+        """
+        Triggers the PredictorThread in order to classify the audio data recorded by the User.
+
+        :return: None
+        """
         self.analyze_label.set("Analyzing...")
 
         pred = PredictionThread(predictor=self.predictor,
@@ -42,20 +47,41 @@ class Application:
         pred.start()
 
     def after_analyze(self, emotional_state):
+        """
+        Updates the GUI once a prediction has been made.
+
+        :param emotional_state:     the string output of the Predictor.make_single_prediction method
+        :return: None
+        """
         self.analyze_label.set("Analyze")
         showinfo("Emotional State", emotional_state + "    ")
 
     def on_record(self):
+        """
+        Triggers the RecorderThread in order to record the User's voice
+
+        :return: None
+        """
         self.record_label.set("Recording...")
 
         recorder = RecorderThread(self.after_record)
         recorder.start()
 
     def after_record(self):
+        """
+        Updates the GUI once the recording is complete.
+
+        :return: None
+        """
         self.record_label.set("Re-Record")
         self.button_2['state'] = "normal"
 
     def build_strings(self):
+        """
+        Declares the String labels for many of the prompts and buttons used in this GUI.
+
+        :return: None
+        """
         self.window_title = "Voice Emotion Recognizer"
         self.main_label = "How are you Feeling today?"
         self.quit_label = "Quit"
