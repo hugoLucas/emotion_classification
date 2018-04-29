9ea354125cd1cd351aa7cf8bea70876d9487b060
hugolucas <hugojlucas@gmail.com>
2018-04-29 16:39:32 -0700

    added comments

--- data.py
+++ data.py
@@ -27,34 +27,55 @@ def process_audio(audio_data, sr):
 
 
 def pad_array(audio_data):
+    """
+    Pads an array with zeros if the audio file used to generate the array is less than a certain value.
+
+    :param audio_data:  a [1, N] dimensional array created by the Librosa.core.load method
+    :return: the input array padded with 0's if necessary
+    """
     if audio_data.shape[0] < 51:
         audio_data = concatenate((audio_data, zeros((51 - audio_data.shape[0], 36))))
     return audio_data
 
 
 class AudioData(Dataset):
+
+    """
+    Implementation of the pytorch.utils.data.Dataset class. Loads and processes the RAVDESS dataset for easier training.
+    """
+
     def __init__(self, configs, training_data=True):
+        """
+        :param configs:         Bunch object of this run's configuration file
+        :param training_data:   if True this object will load in data to be used for training a model, if False the
+                                object will load data for testing a model
+        """
         self.configs = configs
         self.training_data = training_data
         self.file_list, self.dir = self.load_files()
         self.max_samples = None
 
     def __getitem__(self, index):
+        """
+        Takes a random file path from file_list and loads in the corresponding data file. Extracts MFCC features from
+        the audio data as well as the label number from the file name. Pads the audio data if required.
+
+        :param index:   a random index in file_list
+        :return:        a Numpy array containing MFCC features of the audio file as well as the correct label for said
+                        file
+        """
         file_name = self.file_list[index]
         audio_data, sr = load(path.join(self.dir, file_name), duration=self.configs.audio_max_length)
 
-        # Extract audio features
         audio_data = process_audio(audio_data, sr)
         audio_data = pad_array(audio_data)
 
-        # Extract label out, subtract one to make first label 0 rather than 1
         file_name = self.file_list[index].split()[-1]
+
+        # Subtract one as file labels range from 1 to 8 while Softmax function only takes in 0 to 7
         label = int(file_name.split('-')[self.configs.emotion_index]) - 1
 
         return audio_data, label
-
-    def __len__(self):
-        return len(self.file_list)
 
     def load_files(self):
         """
@@ -69,3 +90,6 @@ class AudioData(Dataset):
         file_list = list(filter(lambda f: self.configs.file_type in f, listdir(repo)))
 
         return file_list, repo
+
+    def __len__(self):
+        return len(self.file_list)
