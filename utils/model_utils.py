8eb6fd6213f31431c72f920872faa4c090d11498
hugolucas <hugojlucas@gmail.com>
2018-04-29 17:33:50 -0700

    added comments

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -9,6 +9,7 @@ from torch.cuda import is_available
 from torch import load as py_load
 
 
+# Label to emotional state map
 emotions = {
     0: "Neutral",
     1: "Calm",
@@ -22,10 +23,22 @@ emotions = {
 
 
 def get_emotion(output_index):
+    """
+    Given the model's output, method will return string representation of the index.
+    :param output_index:    a number from 0 to 7.
+    :return:                string for the label emotion
+    """
     return emotions.get(output_index, "ERROR")
 
 
 def load_data_from_path(data_path, audio_duration=5):
+    """
+    Loads in an audio file given that file's full path.
+
+    :param data_path:           path to .wav file
+    :param audio_duration:      the max length to load, five seconds by default
+    :return:                    numpy array of the loaded file
+    """
     audio_data, sr = lib_load(data_path, duration=audio_duration)
     audio_data = process_audio(audio_data, sr)
     audio_data = pad_array(audio_data)
@@ -34,6 +47,12 @@ def load_data_from_path(data_path, audio_duration=5):
 
 
 def to_variable(audio_data):
+    """
+    Takes in a numpy array and converts into a PyTorch Variable.
+
+    :param audio_data:  a numpy array
+    :return:            Variable object of numpy array
+    """
     x = Variable(from_numpy(audio_data))
     x = x.type(FloatTensor)
     x = x.unsqueeze(0)
@@ -42,7 +61,14 @@ def to_variable(audio_data):
 
 
 class Predictor:
+    """
+    Class loads in a previously created model's state and allows for the analysis of a single audio sample at a time.
+    """
     def __init__(self, config_path, model_state):
+        """
+        :param config_path: path to the configuration file used to create saved model
+        :param model_state: path to .pt file output by Pytorch's save method
+        """
         self.configs, _ = get_config_from_json(config_path)
         self.model = BidirectionalLSTM(model_configs=self.configs)
 
@@ -54,6 +80,13 @@ class Predictor:
         self.model.eval()
 
     def make_single_prediction(self, audio_data):
+        """
+        Given a Variable object, method will pass data to model and output a String representation of the model's
+        output.
+
+        :param audio_data:  a Pytorch Variable object
+        :return:            string of emotional state detected
+        """
         output = self.model(audio_data)
         output = softmax(output)
         val, index = max(output, 1)
