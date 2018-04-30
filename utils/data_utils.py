131f7aebb4f9500b29149810e65b80106ebdf2b1
hugolucas <hugojlucas@gmail.com>
2018-04-29 17:22:01 -0700

    added comments

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -2,8 +2,20 @@ from os import path, listdir, makedirs, rename, rmdir
 from utils.config_utils import get_config_from_json
 from random import random
 
+"""
+This script should be used to create a training and test set from the extracted folder of the 
+Audio_Speech_Actors_01-24.zip file from The Ryerson Audio-Visual Database of Emotional Speech and Song (RAVDESS) 
+web page. As of April 29, 2018 this file can be found at: https://zenodo.org/record/1188976
+"""
+
 
 def create_directories(configs):
+    """
+    Creates a directory for the training and test sets if they do not already exist.
+
+    :param configs:     a Bunch object of the run's configuration file
+    :return:            the paths to the training and test directories
+    """
     train_directory, test_directory = configs.data_dir, configs.test_dir
     if not path.isdir(train_directory):
         makedirs(train_directory)
@@ -14,10 +26,24 @@ def create_directories(configs):
 
 
 def move_file(folder_path, destination_path, file_name):
+    """
+    Moves an audio file from its directory to either the train or test set directory.
+
+    :param folder_path:         the path to the directory containing the audio file
+    :param destination_path:    the path to the test or train set directory
+    :param file_name:           the name of the audio file
+    :return:                    None
+    """
     rename(path.join(folder_path, file_name), path.join(destination_path, file_name))
 
 
 def process_folders(configs):
+    """
+    Iterates through a directory containing folders of audio data and segments them into a training and test set.
+
+    :param configs:     a Bunch object of the run's configuration file
+    :return:            None
+    """
     train_directory, test_directory = create_directories(configs)
     for folder in filter(lambda v: "." not in v, listdir(configs.raw_data_dir)):
         folder_path = path.join(configs.raw_data_dir, folder)
